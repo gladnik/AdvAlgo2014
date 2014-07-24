0f2f5d9feb4c63661a03ee11547a8d46eb24d18d
Nikolai Gladkov <nickgladkov@ya.ru>
2014-07-24 17:05:44 +0900

    Assignment 1
    Solovay-Strassen primality test.

---
+++ PrimalityTesting_SolovayStrassen/PrimalityTesting_SolovayStrassen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace PrimalityTesting_SolovayStrassen
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime start, end;
+            start = DateTime.Now;
+            Console.WriteLine(SolovayStrassen.Test(BigInteger.Pow(2, 9941) - 1));
+            end = DateTime.Now;
+            Console.WriteLine((end - start).ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrimalityTesting_SolovayStrassen/PrimalityTesting_SolovayStrassen/SolovayStrassen.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Numerics;
+
+namespace PrimalityTesting_SolovayStrassen
+{
+    class SolovayStrassen
+    {
+        private static Random random = new Random();
+
+        private static BigInteger GetRandomInRange(BigInteger min, BigInteger max) // min <= result <= max
+        {
+            BigInteger result;
+            byte[] bytes = max.ToByteArray();
+            do
+            {
+                random.NextBytes(bytes);
+                bytes[bytes.Length - 1] &= (byte)0x7F; //force sign bit to positive
+                result = new BigInteger(bytes);
+            }
+            while (result > max || result < min);
+
+            return result;
+        }
+
+        private static int JacobiSymbol(BigInteger a, BigInteger n) //Outputs only 1 or -1 as we check GCD separetly
+        {
+            bool flag = true;
+            int shift;
+            BigInteger remainder, temp;
+            do
+            {
+                shift = 0;
+                while ((a >> shift).IsEven)
+                {
+                    shift++;
+                }
+                a >>= shift;
+                if ((shift & 1) == 1) //odd
+                {
+                    remainder = BigInteger.Remainder(n, 8);
+                    if (remainder == 3 || remainder == 5)
+                    {
+                        flag = !flag;
+                    }
+                }
+                if (BigInteger.Remainder(a, 4) == 3 && BigInteger.Remainder(n, 4) == 3)
+                {
+                    flag = !flag;
+                }
+
+                temp = a;
+                a = BigInteger.Remainder(n, temp);
+                n = temp;
+            }
+            while (a > 0);
+
+            return flag ? 1 : -1;
+        }
+
+        public static bool Test(BigInteger n)
+        {
+            if (n == 2) return true;
+            else if (n < 2 || n.IsEven) return false;
+            BigInteger a = GetRandomInRange(2, n - 1);
+            if (BigInteger.GreatestCommonDivisor(a, n) != 1) return false;
+            else return BigInteger.ModPow(a, n >> 1, n) == BigInteger.Remainder(JacobiSymbol(a, n) + n, n);
+        }
+    }
+}
